8641e77c3d20c6d4bb3c5e5289b0c99cc4018744
George <biadedigital1@gmail.com>
2021-12-02 05:32:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Frontend Bootcamp Website - Bootstrap 5
+
+> Simple one page website for a fake bootcamp
+
+
+### Theme Details
---
+++ form/contact.php
@@ -0,0 +1,19 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "contact@biadedigital.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first-name'];
+    $last_name = $_POST['last-name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $first_name . " " . $last_name . " " . $email . " " . $phone . " " " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
